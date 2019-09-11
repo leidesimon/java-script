4e5cdab3580702cb5472b2deaa80f043a4d07fef
leidesimon <leide.simon1@gmail.com>
2019-09-11 12:02:57 -0300

    js exercicios

---
+++ script.js
@@ -0,0 +1,113 @@
+//Decalrando variáveis
+var variavel1 = "se lá";
+let variavel2 = "estação hack";
+const variavel3 = 12;
+
+//Atribuição é = "estação hack"
+
+//Tipos de dados
+
+let exemplo;
+
+String
+exemplo = "Leide";
+exemplo = 21;
+exemplo = 1.60;
+exemplo = true;
+exemplo = false;
+
+let SaboresDePizza
+
+SaboresDePizza = [ "brocolis", "baiana", "portuguesa", "mussarela", "strogonoff"];
+
+let Fagner = ["Mauricio", 18, true, "Santana", ["pé de moleque", "mouse", "pudim"] ];
+
+let fayra = {
+    "sobrenome": "Miranda",
+    "idade": 25,
+    "viva": true,
+    "cidade":"Osasco",
+    "hobbies" : ["dança", "desenho", "leitura"]
+ };
+
+// Estrutura
+// SE ou Nao
+// Condição
+// if (condicao){
+//     codigo executado se a condicao for verdadeira
+
+// }
+// else{
+//     codigo a ser executado for falsa
+// }
+
+// if( condicao){
+
+// }else if(condicao){
+
+// }else{
+    
+// }
+
+//Entrada
+
+    //prompt
+
+//Saida
+
+    // alert
+    // console.log
+
+        //Login: Aluno
+        //Senha: alucinacao
+
+
+
+
+let usuario = "aluno";
+let senha = "alucinacao";
+// let saldo = 1000;
+
+let putUsuario = prompt ("digite o seu usuário");
+let putSenha =  prompt ("Digite a sua senha")
+
+let Tentativas = 0;
+
+while(Tentativa <=3){
+    let usuarioDigitado = prompt ("usuário");
+    let senhaDigitada =  prompt ("senha");
+
+if (usuario==putUsuario && senha==putSenha){
+
+    alert ("Bem vindo, você logou!")
+} else{
+    alert ("Sinto muito, você errou.")
+}
+
+// let saque = Number(prompt("Digite o quanto quer sacar"))
+
+// if (saldo>=saque){
+//         alert( "Saque realizado com sucesso")
+// }else{
+//     alert("Saldo insuficiente")
+// }
+
+// let usuario = aluno;
+// let senha = alucinacao;
+
+// let putUsuario = prompt ("digite o seu usuário");
+// let putSenha =  prompt ("Digite a sua senha")
+
+// if (usuario==putUsuario){
+    // let putUsuario = prompt ("digite o seu usuário");
+        //alert ("Bem vindo, você logou!")
+// if(senha==putSenha)
+// } else{
+//     alert ("Sinto muito, você errou.")
+
+//Template String
+
+//Estrutura de repetição
+//ENQUANTO
+// While (condição) {
+    //codigo a ser repetido
